b572f4bc9bafea3725b714fb75dd44d59ee31156
MotoyaAsahina <motoyaasahina@icloud.com>
2024-07-04 02:59:29 +0900

    implement notification repository

--- src/main/java/tech/asari/todo/reposiotry/NotificationRepository.java
+++ src/main/java/tech/asari/todo/reposiotry/NotificationRepository.java
@@ -1,5 +1,7 @@
 package tech.asari.todo.reposiotry;
 
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import tech.asari.todo.reposiotry.domain.Notification;
 import tech.asari.todo.reposiotry.domain.NotificationTime;
@@ -11,34 +13,68 @@ import java.util.List;
 @Repository
 public class NotificationRepository implements INotificationRepository {
 
+    private final JdbcClient client;
+
+    public NotificationRepository(JdbcClient client) {
+        this.client = client;
+    }
+
     @Override
     public List<Timestamp> createNotificationTimesIfNotExists(List<NotificationTime> notificationTimes) {
-        return List.of();
+        return notificationTimes.stream().filter(notificationTime -> {
+            try {
+                client.sql("INSERT INTO notification_times (time) VALUES (:time)")
+                        .param("time", notificationTime.time())
+                        .update();
+            } catch (DuplicateKeyException e) {
+                return false;
+            }
+            return true;
+        }).map(NotificationTime::time).toList();
     }
 
     @Override
     public void createNotifications(List<Notification> notifications) {
-
+        notifications.forEach(notification -> {
+            client.sql("INSERT INTO notifications (time, task_id, tag) VALUES (:time, :task_id, :tag)")
+                    .param("time", notification.time())
+                    .param("task_id", notification.taskId())
+                    .param("tag", notification.tag())
+                    .update();
+        });
     }
 
     @Override
     public List<Task> getTasksByNotificationTime(Timestamp time) {
-        return List.of();
+        return client.sql("SELECT * FROM tasks WHERE id IN (SELECT task_id FROM notifications WHERE time = :time)")
+                .param("time", time)
+                .query(Task.class)
+                .list();
     }
 
     @Override
     public void deleteNotifications(int taskId) {
-
+        client.sql("DELETE FROM notifications WHERE task_id = :task_id")
+                .param("task_id", taskId)
+                .update();
     }
 
     @Override
     public List<Timestamp> getNotificationTimesNotNoticed() {
-        // NOTE: 現在時刻よりも後の時間のみ取得する
-        return List.of();
+        return client.sql("SELECT time FROM notification_times WHERE time > NOW()")
+                .query(Timestamp.class)
+                .list();
     }
 
     @Override
     public void setNotificationTimeNoticed(Timestamp time, int taskCount) {
-
+        client.sql("""
+                        UPDATE notification_times
+                        SET noticed = TRUE, noticed_at = NOW(), task_count = :task_count
+                        WHERE time = :time
+                        """)
+                .param("time", time)
+                .param("task_count", taskCount)
+                .update();
     }
 }
