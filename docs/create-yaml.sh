fca3f853bff26bef20a748970970aef35ac0426d
MotoyaAsahina <motoyaasahina@icloud.com>
2024-07-10 11:49:37 +0900

    fix swagger

--- docs/create-yaml.sh
+++ docs/create-yaml.sh
@@ -6,4 +6,4 @@ curl http://localhost:8010/v3/api-docs.yaml -o $BASEDIR/api-docs.yaml
 
 sed -i '' -e 's/'"'"'\*\/\*'"'"'/application\/json/g' $BASEDIR/api-docs.yaml
 sed -i '' -e 's/ResponseTask/Task/g' -e 's/ResponseTag/Tag/g' -e 's/ResponseGroup/Group/g' $BASEDIR/api-docs.yaml
-sed -i '' -e 's/task-controller/task-api/g' -e 's/tag-controller/tag-api/g' -e 's/group-controller/group-api/g' $BASEDIR/api-docs.yaml
+sed -i '' -e 's/task-controller/task/g' -e 's/tag-controller/tag/g' -e 's/group-controller/group/g' $BASEDIR/api-docs.yaml
